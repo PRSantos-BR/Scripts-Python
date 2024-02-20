bbaff8b8f8dda4de7d7c9c052d9095de0a9162f2
PRSantos-BR <59713966+PRSantos-BR@users.noreply.github.com>
2024-02-20 14:26:10 -0300

    Update validacao_carga_apis.py

--- validacao_carga_apis.py
+++ validacao_carga_apis.py
@@ -1,11 +1,12 @@
 import argparse
 import configparser
-import json
 import requests
+import json
 import json
 import cx_Oracle as cnnOracle
 
 """Devemos melhorar para tornar esse script genérico"""
+
 
 def valida_alerta(url: str, end_point: str, token: str, numero_telefone: str) -> str:
     headers_api = {'Content-Type': 'application/json',
@@ -15,7 +16,7 @@ def valida_alerta(url: str, end_point: str, token: str, numero_telefone: str) ->
                    'user-agent': 'curl/7.60.0',
                    'x-client-auth': 'Bearer {}'.format(token)}
 
-    data_app = {"data": {"customer": {"networkMsisdn": numero_telefone}}}
+    data_app: dict = {"data": {"customer": {"networkMsisdn": numero_telefone}}}
 
     resp = requests.post(url + end_point,
                          headers=headers_api,
@@ -37,95 +38,102 @@ def valida_cpf_custom_score(url: str, end_point: str, token: str, numero_cpf: st
                    'document': numero_cpf,
                    'score': cliente['nome_score']}
 
-    data_app = {}
+    data_app: dict = {}
 
     resp = requests.post(url + end_point,
                          headers=headers_api,
                          data=data_app)
-    
-    numero_cpf = '{0}.{1}.{2}-{3}'.format(numero_cpf[:3], numero_cpf[3:6], numero_cpf[6:9], numero_cpf[9:])
+
+    numero_cpf: str = '{0}.{1}.{2}-{3}'.format(numero_cpf[:3], numero_cpf[3:6], numero_cpf[6:9], numero_cpf[9:])
     if resp.status_code == 200:
-        valor_score = str(json.loads(resp.text)['score'][cliente['nome_score']]).rjust(6, chr(32))
-        return 'Número CPF: {0} / Score ({1}): {2}'.format(numero_cpf, 
-                                                           [tipo_score for tipo_score in json.loads(resp.text)['score'].keys()][0],
+        valor_score: str = str(json.loads(resp.text)['score'][cliente['nome_score']]).rjust(6, chr(32))
+        return 'Número CPF: {0} / Score ({1}): {2}'.format(numero_cpf,
+                                                           [tipo_score for tipo_score in
+                                                            json.loads(resp.text)['score'].keys()][0],
                                                            valor_score)
     else:
-        return 'Número CPF: {0} / Codigo Erro: {1}'.format(numero_cpf, 
+        return 'Número CPF: {0} / Codigo Erro: {1}'.format(numero_cpf,
                                                            resp.status_code)
 
 
 def valida_cpf_score(url: str, end_point: str, token: str, numero_cpf: str) -> str:
     headers_api = {'Content-Type': 'application/json',
                    'X-CustomerID': 'Fulano de Tal',
-                   'cache-control': 'no-cache', 
-                   'user-agent': 'curl/7.60.0', 
+                   'cache-control': 'no-cache',
+                   'user-agent': 'curl/7.60.0',
                    'x-client-auth': 'Bearer {0}'.format(token)}
-                   
-    data_app = {'data':{'customer': {'idCpf': numero_cpf}}}
-    
+
+    data_app: dict = {'data': {'customer': {'idCpf': numero_cpf}}}
+
     resp = requests.post(url + end_point,
                          headers=headers_api,
                          data=json.dumps(data_app))
 
-    numero_cpf = data_app['data']['customer']['idCpf']
-    numero_cpf = '{0}.{1}.{2}-{3}'.format(numero_cpf[:3], numero_cpf[3:6], numero_cpf[6:9], numero_cpf[9:])
+    numero_cpf: str = data_app['data']['customer']['idCpf']
+    numero_cpf: str = '{0}.{1}.{2}-{3}'.format(numero_cpf[:3], numero_cpf[3:6], numero_cpf[6:9], numero_cpf[9:])
     if resp.status_code == 200:
-        valor_score = str(json.loads(resp.text)['data']['statsResult']['score']).rjust(4, chr(32))
-        return 'Número CPF: {0} / Score: {1}'.format(numero_cpf, 
+        valor_score: str = str(json.loads(resp.text)['data']['statsResult']['score']).rjust(4, chr(32))
+        return 'Número CPF: {0} / Score: {1}'.format(numero_cpf,
                                                      valor_score)
     else:
-        return 'Número CPF: {0} / Codigo Erro: {1}'.format(numero_cpf, 
-                                                    resp.status_code)
+        return 'Número CPF: {0} / Codigo Erro: {1}'.format(numero_cpf,
+                                                           resp.status_code)
 
 
-def valida_endereco(url: str, end_point: str, token: str, numero_cep: str, numero_endereco: str, numero_cpf: str, numero_telefone: str) -> str:
-    headers_api = {'Content-Type': 'application/json',
-                   'X-CustomerID': 'Fulano de Tal',
-                   'cache-control':'no-cache',
-                   'user-agent':'curl/7.60.0',
-                   'x-client-auth': 'Bearer {0}'.format(token)}
+def valida_endereco(url: str, end_point: str,
+                    token: str,
+                    numero_cep: str,
+                    numero_endereco: str,
+                    numero_cpf: str,
+                    numero_telefone: str) -> str:
+    headers_api: dict = {'Content-Type': 'application/json',
+                         'X-CustomerID': 'Fulano de Tal',
+                         'cache-control': 'no-cache',
+                         'user-agent': 'curl/7.60.0',
+                         'x-client-auth': 'Bearer {0}'.format(token)}
 
-    data_app = {"data": {"customer": {"addressZipCode": numero_cep, "addressNumber": numero_endereco, "cpf": numero_cpf, "networkMsisdn": numero_telefone}}}
+    data_app: dict = {"data": {"customer": {"addressZipCode": numero_cep,
+                                            "addressNumber": numero_endereco,
+                                            "cpf": numero_cpf,
+                                            "networkMsisdn": numero_telefone}}}
 
     resp = requests.post(url + end_point,
                          headers=headers_api,
                          data=json.dumps(data_app))
 
     if resp.status_code != 200:
-        return 'Valida ' + str( resp.text )
+        return 'Valida ' + str(resp.text)
     else:
         return 'Erro: ' + str(resp.text)
 
 
 def valida_telefone(url: str, end_point: str, token: str, numero_telefone: str, numero_cpf: str) -> str:
-    headers_api = {'Content-Type': 'application/json',
-                   'X-CustomerID': 'Fulano de Tal',
-                   'cache-control':'no-cache',
-                   'user-agent':'curl/7.60.0',
-                   'x-client-auth': 'Bearer {0}'.format(token)}
+    headers_api: dict = {'Content-Type': 'application/json',
+                         'X-CustomerID': 'Fulano de Tal',
+                         'cache-control': 'no-cache',
+                         'user-agent': 'curl/7.60.0',
+                         'x-client-auth': 'Bearer {0}'.format(token)}
 
-    data_app = {"data": {"customer": {"networkMsisdn": numero_telefone , "cpf": numero_cpf}}}
+    data_app: dict = {"data": {"customer": {"networkMsisdn": numero_telefone, "cpf": numero_cpf}}}
 
     resp = requests.post(url + end_point,
                          headers=headers_api,
                          data=json.dumps(data_app))
-    
-    # Número Linha: 55999999999 / Número CPF: 9999999999 / {"data":{"result":{"summaryMessage":"Customer found.","detailedMessage":"CPF does not belong to customer’s MSISDN."}}}
 
-    # Número Linha: 55999999999 / Número CPF: 9999999999 / {"data":{"result":{"summaryMessage":"Customer found.","detailedMessage":"CPF does not belong to customer’s MSISDN."}}}
-
-    numero_telefone = json.loads(resp.request.body)['data']['customer']['networkMsisdn']
-    numero_cpf = json.loads(resp.request.body)['data']['customer']['cpf']
+    numero_telefone: str = json.loads(resp.request.body)['data']['customer']['networkMsisdn']
+    numero_cpf: str = json.loads(resp.request.body)['data']['customer']['cpf']
     if resp.status_code == 200:
         return 'Número Linha: {0} / Número CPF: {1} / {2} / {3}'.format(numero_telefone,
                                                                         numero_cpf,
-                                                                        json.loads(resp.text)['data']['result']['summaryMessage'],
-                                                                        json.loads(resp.text)['data']['result']['detailedMessage'])
+                                                                        json.loads(resp.text)['data']['result'][
+                                                                            'summaryMessage'],
+                                                                        json.loads(resp.text)['data']['result'][
+                                                                            'detailedMessage'])
     else:
-        return 'Número Linha: {0} / Número CPF: {1} / Status: {2}'.format(numero_telefone, 
-                                                                                numero_cpf, 
-                                                                                resp.status_code)
- 
+        return 'Número Linha: {0} / Número CPF: {1} / Status: {2}'.format(numero_telefone,
+                                                                          numero_cpf,
+                                                                          resp.status_code)
+
 
 def recupera_amostra() -> list:
     try:
@@ -133,9 +141,9 @@ def recupera_amostra() -> list:
         dados_conexao: dict = json.loads(cfg['DADOS_CONEXOES']['SERVIDOR'])
 
         cnn_SERVIDOR = cnnOracle.connect(user=dados_conexao['usuario'],
-                                       password=dados_conexao['senha'],
-                                       dsn=dados_conexao['dsn'],
-                                       encoding='UTF-8')
+                                         password=dados_conexao['senha'],
+                                         dsn=dados_conexao['dsn'],
+                                         encoding='UTF-8')
 
         # Fabrica CURSOR
         crs_ = cnn_SERVIDOR.cursor()
@@ -175,7 +183,6 @@ def recupera_amostra() -> list:
                                             tabela['tabela'],
                                             argumentos.total_amostra)
 
-        
         # Lista contendo amostra de CPF's
         crs_.execute(query)
 
@@ -185,10 +192,10 @@ def recupera_amostra() -> list:
         # Fecha Cursor e Conexão
         crs_.close()
         cnn_SERVIDOR.close()
-    
+
         # Retorna amostra
         return amostra
-        
+
     except cnnOracle.DataError as e:
         print('Erro DataError', e)
     except cnnOracle.DatabaseError as e:
@@ -203,20 +210,20 @@ def recupera_amostra() -> list:
 
 def recupera_amostra_teste() -> list:
     # Valida Telefone 
-    amostra = [('99999977777', '99999777777')]
+    amostra_test: list = [('99999977777', '99999777777'), ]
 
     # Valida Alerta
     """
-    amostra = [('99999977777', '99999777777'), ]
+    amostra_test: list = [('99999977777', '99999777777'), ]
     """
 
     # Score e Score Customizado
     """
-    amostra = [('00000000000', )]
+    amostra_test: list = [('00000000000', )]
     """
 
     # Retorna amostra
-    return amostra
+    return amostra_test
 
 
 parser = argparse.ArgumentParser(description='Valida carga de APIs')
@@ -235,50 +242,50 @@ cliente: dict = json.loads(cfg['CLIENTES'][argumentos.sigla_cliente])
 tabela: dict = json.loads(cfg['TABELAS_APIS'][argumentos.nome_api])
 
 # Gera Token
-headers_token = {'Content-Type': 'application/x-www-form-urlencoded',
-                 'cache-control': 'no-cache',
-                 'x-client-auth': 'Basic {0}'.format(cliente['senha'])}
-                    
+headers_token: dict = {'Content-Type': 'application/x-www-form-urlencoded',
+                       'cache-control': 'no-cache',
+                       'x-client-auth': 'Basic {0}'.format(cliente['senha'])}
+
 data_token = {'grant_type': 'client_credentials'}
-respToken = requests.post(url='https://api.claro.com.br/oauth2/v1/token', 
-                          headers=headers_token, 
+respToken = requests.post(url='https://api.claro.com.br/oauth2/v1/token',
+                          headers=headers_token,
                           data=data_token)
 
 # 
 if respToken.status_code == 200:
     # Recuperando amostra de dados da base ORACLE
-    amostra = recupera_amostra_teste()
-    # amostra = recupera_amostra()
+    amostra: list = recupera_amostra_teste()
+    # amostra: list = recupera_amostra()
 
     # Itera sobre a lista de amostra de CPF's
     for parametro in amostra:
         if argumentos.nome_api == 'ALERT':
-            print(valida_alerta(url='https://api.claro.com.br', 
+            print(valida_alerta(url='https://api.claro.com.br',
                                 end_point='/customers/v1/alerts',
                                 token=json.loads(respToken.text)['access_token'],
                                 numero_telefone='55' + str(parametro[0])))
         elif argumentos.nome_api == 'CUSTOMSCORES':
-            print(valida_cpf_custom_score(url='https://api.claro.com.br', 
-                                          end_point='/customers/v1/customscores', 
-                                          token=json.loads(respToken.text)['access_token'], 
+            print(valida_cpf_custom_score(url='https://api.claro.com.br',
+                                          end_point='/customers/v1/customscores',
+                                          token=json.loads(respToken.text)['access_token'],
                                           numero_cpf=parametro[0]))
         elif argumentos.nome_api == 'SCORE':
-            print(valida_cpf_score(url='https://api.claro.com.br', 
+            print(valida_cpf_score(url='https://api.claro.com.br',
                                    end_point='/customers/v1/scores',
                                    token=json.loads(respToken.text)['access_token'],
                                    numero_cpf=parametro[0]))
         elif argumentos.nome_api == 'VALIDATIONS_ADDRESS':
-            print(valida_endereco(url='https://api.claro.com.br', 
-                                  end_point='/customers/v2/validationsaddress', 
-                                  token=json.loads(respToken.text)['access_token'], 
+            print(valida_endereco(url='https://api.claro.com.br',
+                                  end_point='/customers/v2/validationsaddress',
+                                  token=json.loads(respToken.text)['access_token'],
                                   addressZipCode=parametro[0],
                                   addressNumber=parametro[1],
                                   numero_cpf=parametro[2],
                                   networkMsisdn=parametro[3]))
         elif argumentos.nome_api == 'VALIDATIONS_PHONES':
-            print(valida_telefone(url='https://api.claro.com.br', 
-                                  end_point='/customers/v1/validationsphones', 
-                                  token=json.loads(respToken.text)['access_token'], 
+            print(valida_telefone(url='https://api.claro.com.br',
+                                  end_point='/customers/v1/validationsphones',
+                                  token=json.loads(respToken.text)['access_token'],
                                   numero_telefone='55' + str(parametro[0]),
                                   numero_cpf=parametro[1]))
 else:
